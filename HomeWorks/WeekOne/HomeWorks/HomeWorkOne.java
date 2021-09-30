365c77108a5e2bd6442721188c0336ca7675ad22
Erim Serdönmez <eserdnmez@gmail.com>
2021-09-30 20:01:44 +0300

    HomeWork First Done

--- HomeWorks/WeekOne/HomeWorks/HomeWorkOne.java
+++ HomeWorks/WeekOne/HomeWorks/HomeWorkOne.java
@@ -1,7 +1,50 @@
+/** @author Erim Serdonmez -- 30/09/2021  
+ *  WeekOne.java -- Write a program that prints out characters in the form of a grid. The program should ask a number N from the user. The grid must be drawn as a square whose edge is N characters. The outermost characters must be * characters describing a border. Inside the border a checkerboard must be drawn using space and + characters. The top-left corner checkerboard character must be + character. Below are the outputs for 5 different values of N.
+ */
+
 package WeekOne.HomeWorks;
+
+import java.util.Scanner;
 
 public class HomeWorkOne {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner input = new Scanner(System.in);
+        // Getting input from the user
+        System.out.print("Please enter a Number : ");
+        int prompt = input.nextInt();
+        input.close();
+
+        // Variables
+        String firstRow = "";
+        String lastRow = "";
+        String body = "";
+
+        // Main Loop
+        for (int i = 0; i < prompt; i++) {
+            if (prompt > 1) {
+                lastRow += "*";
+            }
+            firstRow += "*";
+
+            if (i >= prompt - 2)
+                continue;
+
+            body += "*";
+            for (int j = 0; j < (prompt - 2); j++) {
+                if (i % 2 == 1 && j % 2 == 0) {
+                    body += " ";
+                } else if (i % 2 == 1 && j % 2 == 1) {
+                    body += "+";
+                } else if (i % 2 == 0 && j % 2 == 1) {
+                    body += " ";
+                }else{
+                    body += "+";
+                }
+            }
+            body += "*\n";
+        }
+        // printing the table
+        String finalTable = firstRow + "\n" + body + lastRow;
+        System.out.println(finalTable);
     }
 }
