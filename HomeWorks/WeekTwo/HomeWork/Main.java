d77ce3d61eee1f7c33c89284ed9d6cc704c31c31
Erim Serdönmez <eserdnmez@gmail.com>
2021-10-18 19:34:21 +0300

    Edit

--- HomeWorks/WeekTwo/HomeWork/Main.java
+++ HomeWorks/WeekTwo/HomeWork/Main.java
@@ -3,18 +3,18 @@ package HomeWork;
 import java.util.Scanner;
 
 public class Main {
-    public static void askCustomer(Cart cart){
+    public static void askCustomer(Cart cart) {
 
         Scanner datas = new Scanner(System.in);
-        while(true){
+        while (true) {
             System.out.println("If you want to buy some product enter 1, If you do not please enter 0");
             int user = datas.nextInt();
 
-            if(user == 0 ){
+            if (user == 0) {
                 System.out.println(cart.toString());
                 System.out.println("In total you have to pay " + cart.totalPrice() + " TL.");
                 break;
-            }else{
+            } else {
                 System.out.println("What's the product name: ");
                 String pName = datas.nextLine();
 
@@ -26,35 +26,38 @@ public class Main {
                 datas.nextLine();
 
                 System.out.println("How many products do you want to buy: ");
-                int numberOfProduct = dats.nextInt();
+                int numberOfProduct = datas.nextInt();
 
-                System.out.println("Is there any discount on your product if there is please enter the amount, if not enter 0 ?");
+                System.out.println(
+                        "Is there any discount on your product if there is please enter the amount, if not enter 0 ?");
                 double userSelection_D = datas.nextDouble();
                 datas.nextLine();
 
                 System.out.println("Does product have buy2Take3 advantage? (y)(n)");
                 String userSelection_B = datas.nextLine();
 
-                Product product = new Product(seller,pName,price);
+                Product product = new Product(seller, pName, price);
 
-                if(userSelection_D > 0 && userSelection_B.equals("y")){
+                if (userSelection_D > 0 && userSelection_B.equals("y")) {
                     System.out.println("Please choose one option!!!");
                 }
-                if(userSelection_D != 0 && userSelection_B.equals("n")){
+                if (userSelection_D != 0 && userSelection_B.equals("n")) {
                     Product p = new DiscountedProduct(product, userSelection_D);
                     cart.addProduct(p, numberOfProduct);
                     continue;
                 }
 
-                if(userSelection_B.equals("y") && (userSelection_D == 0)){
+                if (userSelection_B.equals("y") && (userSelection_D == 0)) {
                     Product p = new Buy2Take3product(product);
                     cart.addProduct(p, numberOfProduct);
                     continue;
                 }
                 cart.addProduct(product, numberOfProduct);
-
-            
+            }
         }
+        datas.close();
+
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to kodluyoruz shop");
@@ -68,7 +71,7 @@ public class Main {
 
         Cart cart = new Cart();
         askCustomer(cart);
-
+        scanner.close();
     }
 
 }
